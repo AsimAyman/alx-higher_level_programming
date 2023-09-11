5357b2f1707711c2725f32deb46cf73bfa1bd8fb
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-09-11 21:06:34 +0300

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,4 +4,4 @@ def new_in_list(my_list, idx, element):
                 return (my_list)
         newList = my_list.copy()
         newList[idx] = element
-        return newList
+        return (newList)
