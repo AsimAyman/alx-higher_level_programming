bf58982a5265be4a44dff81f1799b28b691b4a56
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-09-11 21:42:50 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    slist = list(my_string)
-    [slist.remove(i) for i in slist if i == 'c' or i == 'C']
-    return("".join(slist))
+    s = list(my_string)
+    [s.remove(i) for i in slist if i == 'c' or i == 'C']
+    return("".join(s))
