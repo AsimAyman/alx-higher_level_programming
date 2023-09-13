1814cdd80071912713a1e38e7c6d8c29488d4323
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-09-13 21:36:53 +0300

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+def squared(element):
+    return (element**2)
+
+
 def square_matrix_simple(matrix=[]):
-    n = [[j ** 2 for j in i] for i in matrix]
-    return (n)
+    new = matrix[:]
+    for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                new[i] = list(map(squared, matrix[i]))
+    return new
