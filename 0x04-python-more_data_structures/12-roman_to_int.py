c19e97d3ff8d6500bc582eb12faf341b444032e9
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-09-13 21:34:08 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
-        return (0)
-    Rm = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    rm = roman_string
-    dlist = [Rm[i[0]] if Rm[i[0]] >= Rm[i[1]] else (-1*Rm[i[0]])
-             for i in zip(rm, rm[1:] + rm[-1])]
-    dec = sum(dlist)
-    return (dec)
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    for j in roman_string:
+        if (j != 'I' and j != 'V' and j != 'X' and
+                j != 'L' and j != 'C' and j != 'D' and j != 'M'):
+            return 0
+
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = []
+    for i in range(len(roman_string) - 1, -1, -1):
+        if i == len(roman_string) - 1:
+            largestNum = d[roman_string[i]]
+        if d[roman_string[i]] < largestNum:
+            num.append(largestNum - d[roman_string[i]])
+            num.remove(largestNum)
+            largestNum = d[roman_string[i]]
+        else:
+            num.append(d[roman_string[i]])
+            largestNum = d[roman_string[i]]
+    return sum(num)
