73cefbe50c8e51fa7518cd6cf0ec9ea26696da4d
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-09-13 21:28:58 +0300

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    a_dictionary.pop(key, None)
-    return (a_dictionary)
+def multiply_by_2(a_dictionary):
+    new_dict = {i: 2 * a_dictionary[i] for i in a_dictionary}
+    return (new_dict)
