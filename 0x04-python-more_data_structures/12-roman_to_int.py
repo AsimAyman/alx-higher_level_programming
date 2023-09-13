8c53317237f90425c426e9ad4d3b194ea58de671
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-09-13 21:32:34 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str:
-        return 0
-
-    t = 0
-    n = 0
-    di = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50 ,'C' : 100, 'D' : 500, 'M' 1000 }
-    for r in reversed(roman_string):
-        n = di[r]
-        t += n if t < n * 5 else - n
-    return t    
-    
+    if not roman_string or not isinstance(roman_string, str):
+        return (0)
+    Rm = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    rm = roman_string
+    dlist = [Rm[i[0]] if Rm[i[0]] >= Rm[i[1]] else (-1*Rm[i[0]])
+             for i in zip(rm, rm[1:] + rm[-1])]
+    dec = sum(dlist)
+    return (dec)
