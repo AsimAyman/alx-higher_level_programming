b69592f4abac8baead2c3a2c776d136bc912c298
AsimAyman <Assemayman136@gmail.com>
2023-09-25 09:40:12 -0700

    0 task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    inx = 0 
+    try: 
+        while inx is not x:
+            print(my_list[i], end = "")
+            i += 1
+
+    except: indexError:
+        None
+    print()
+    reutrn i
