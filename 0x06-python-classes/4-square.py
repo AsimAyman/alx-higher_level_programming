515afb0607e4ce122462637fae1ab20250d3e33e
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-09-26 23:05:32 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
-class Square:
+"""Square module."""
 
-    """ Class Square that defines methods and attributes for a square object"""
+
+class Square:
+    """Defines a square."""
 
     def __init__(self, size=0):
-        """ Class Constructor """
         self.size = size
 
     @property
     def size(self):
-        """ Private Attribute size Getter """
-        return (self.__size)
-
+        return self.__size
     @size.setter
     def size(self, value):
-        """ Private Attribute size Setter """
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
-    def area(self):
-        """ Method that calculates current square area """
-        return (self.__size * self.__size)
+        self.__size = value
