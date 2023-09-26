36e81af75619fb05f6efd472c50555c5f1a40d8a
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-09-26 22:56:02 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,20 +3,11 @@ class Square:
     """Class Square that defines a square"""
 
     def __init__(self, size=0):
-        self.size = size
-
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
-
+        """Constructor"""
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size    
     def area(self):
         return self.__size ** 2
