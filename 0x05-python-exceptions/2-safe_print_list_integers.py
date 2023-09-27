2ebbda99f8a715df96619f2dc397283bfd020e34
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-09-27 20:41:38 +0300

    Update 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,6 +10,6 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             pass
         else:
-            count += 1
+            c += 1
     print()
     return (c)
