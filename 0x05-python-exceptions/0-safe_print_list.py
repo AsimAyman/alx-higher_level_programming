eea5ce1068e4459b8a1d07f697f7eb2db2d5b0d5
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-09-27 20:04:14 +0300

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    inx= 0 
+    inx = 0 
     try:
         while inx is not x:
         print(my_list[inx], end= "")
         inx += 1
 
-    except ItndexError:
+    except IndexError:
         None
     print()
-    reutrn inx
+    return inx
