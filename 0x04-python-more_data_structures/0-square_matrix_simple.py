863c769c4a3af6a4c01126d571528f064c248fef
Asim Ayman <83433950+AsimAyman@users.noreply.github.com>
2023-11-08 22:11:43 +0200

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     nw_matrix = matrix.copy()
-    i =0 
-    j =0 
+    i = 0
+    j = 0
     while j < len(nw_matrix):
         while i < len(nw_matrix[j]):
             nw_matrix[j][i] = nw_matrix[j][i] ** 2
