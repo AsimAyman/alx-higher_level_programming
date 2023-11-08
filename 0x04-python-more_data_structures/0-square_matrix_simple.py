6657a608193db212d9b006c67c4e35002e96c86a
AsimAyman <Assemayman136@gmail.com>
2023-11-08 12:06:42 -0800

    mod 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in row] for row in matrix]
-    return new_matrix
+    nw_matrix = matrix.copy()
+    i = 0 
+    j=0 
+    while j < len(nw_matrix):
+        while i <len(nw_matrix[j]):
+            nw_matrix[j][i] = nw_matrix[j][i] ** 2
+            i+=1
+        j +=1
+    return nw_matrix
